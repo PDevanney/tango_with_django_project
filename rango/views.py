0b987125426359197b8e29077a2cd1ff77ff63a4
Patrick Devanney <2329979d@student.gla.ac.uk>
2019-02-03 20:10:25 +0000

    Chapter 9 Complete - With Exercises

--- rango/views.py
+++ rango/views.py
@@ -87,12 +87,14 @@ def register(request):
             user = user_form.save()
             user.set_password(user.password)
             user.save()
+
             profile = profile_form.save(commit=False)
             profile.user = user
+            
             if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
-                profile.save()
-                registered = True
+            profile.save()
+            registered = True
         else:
             print(user_form.errors, profile_form.errors)
     else:
